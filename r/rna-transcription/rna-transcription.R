cbf0fae067f3ecea9c3458a4005977a1618c7223
Cam Warner <cameroncwarner@gmail.com>
2017-09-13 00:28:16 -0400

    RNA Transcription #1

--- r/rna-transcription/rna-transcription.R
+++ r/rna-transcription/rna-transcription.R
@@ -1,3 +1,24 @@
 to_rna <- function(dna) {
+  unlist(convertStrand(strsplit(dna, "")[[1]]));
+}
+
+convertStrand <- function(dNAVector) {
+  idx=1;
+  rna = list();
+  for(dna in dNAVector) {
+    rna[idx] <- convertNucleotide(dna);
+    idx = idx + 1;
+  }
   
+  paste(rna,collapse='');
+}
+
+convertNucleotide <- function(dna) {
+  var.rna = switch(dna,
+         'G' = 'C',
+         'C' = 'G',
+         'T' = 'A',
+         'A' = 'U',
+         stop('Bad input'));
+  print(var.rna);
 }
