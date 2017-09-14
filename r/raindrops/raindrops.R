c536180804ed81f193b6bc52bd49b87a46bb0bf1
Cam Warner <cameroncwarner@gmail.com>
2017-09-14 01:44:09 -0400

    Raindrops #1

--- r/raindrops/raindrops.R
+++ r/raindrops/raindrops.R
@@ -1,3 +1,24 @@
 raindrops <- function(number) {
+  speak = ''
+  if(isFactor(number, 3)) {
+    speak = 'Pling';
+  }
   
+  if(isFactor(number, 5)) {
+    speak = paste(speak, 'Plang', sep='');
+  }
+  
+  if(isFactor(number, 7)) {
+    speak = paste(speak, 'Plong', sep='');
+  }
+  
+  if(speak == '') {
+    speak = paste(speak, number, sep='');
+  }
+  
+  speak;
+}
+
+isFactor <- function(number, divisor) {
+  number %% divisor == 0;
 }
