b4b0141950ae13d6267fe56aea6735464e45a005
Cam Warner <cameroncwarner@gmail.com>
2017-09-14 01:35:12 -0400

    Difference of squares #2

--- r/difference-of-squares/difference-of-squares.R
+++ r/difference-of-squares/difference-of-squares.R
@@ -2,16 +2,5 @@
 # and should return the difference-of-squares as described
 # in the README.md
 difference_of_squares <- function(natural_number) {
-  numbers <- 0:natural_number;
-  sumOfSquares = 0;
-  squareOfSums = 0;
-  
-  for(number in numbers)
-  {
-    squareOfSums = squareOfSums + number;
-    sumOfSquares = sumOfSquares + number ^2;
-  }
-  squareOfSums = squareOfSums ^ 2;
-  
-  print(squareOfSums - sumOfSquares);
+  sum(0:natural_number)^2 - sum((0:natural_number)^2)
 }
