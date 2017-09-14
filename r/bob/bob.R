ce7e9160711a7b8e0d85a0718b15c3cbe2c8bffd
Cam Warner <cameroncwarner@gmail.com>
2017-09-14 02:24:34 -0400

    Bob #1

--- r/bob/bob.R
+++ r/bob/bob.R
@@ -1,3 +1,27 @@
 bob <- function(input) {
+  input = trimws(input)
+  if(silence(input)) {
+    'Fine. Be that way!'
+  } else if(yell(input)) {
+    'Whoa, chill out!'
+  } else if(question(input)) {
+    'Sure.'
+  } else {
+    'Whatever.'
+  }
+}
 
+question <- function(input) {
+  lastChar = substring(input, nchar(input), nchar(input))
+   lastChar == '?'
+}
+
+yell <- function(input) {
+  toupper(input) == input && any(is.letter(input))
+}
+
+is.letter <- function(x) grepl("[[:alpha:]]", x)
+
+silence <- function(input) {
+  nchar(input) == 0
 }
