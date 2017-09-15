f08c73b07de73bd67f6a35d4163f861a5db068ff
Cam Warner <cameroncwarner@gmail.com>
2017-09-15 00:21:56 -0400

    sum-of-multiples #1

--- r/sum-of-multiples/sum-of-multiples.R
+++ r/sum-of-multiples/sum-of-multiples.R
@@ -1,3 +1,11 @@
 sum_of_multiples <- function(factors, limit) {
+  range <- 1:(limit-1)
   
+  isFactor <- function(number) {
+    any(number %% factors == 0)
+  }
+  
+  sum(Filter(isFactor, range))
 }
+
+
