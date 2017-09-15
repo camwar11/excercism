a5dceb5c8b8910d7fe8301086ea65ebdbef9f056
Cam Warner <cameroncwarner@gmail.com>
2017-09-14 23:25:16 -0400

    Anagram #1

--- r/anagram/anagram.R
+++ r/anagram/anagram.R
@@ -1,3 +1,36 @@
 anagram <- function(subject, candidates) {
+  find.anagrams(subject, candidates)
+}
 
+find.anagrams <- function(word1, candidates) {
+  anagrams <- list()
+  idx <- 1
+  for(candidate in candidates) {
+    if(nchar(word1) != nchar(candidate) ||
+       tolower(word1) == tolower(candidate)) {
+      next
+    }
+    
+    chars <- strsplit(tolower(word1), '')[[1]]
+    
+    isAnagram <- TRUE
+    
+    tester = tolower(candidate)
+    for(char in chars) {
+      new.tester <- sub(char, '', tester)
+      if(new.tester == tester) {
+        isAnagram <- FALSE
+        break
+      }
+      
+      tester = new.tester
+    }
+    
+    if(isAnagram && length(chars) > 0)
+      anagrams[idx] <- candidate
+    
+    idx <- idx + 1
+  }
+  
+  return(unlist(anagrams))
 }
